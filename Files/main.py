02045171b6ae2a0f006cc13208afd64c8e7452a6
aditya-mad <maddulapalliaditya@gmail.com>
2023-06-17 22:06:55 +0530

    updated search

--- Files/main.py
+++ Files/main.py
@@ -8,7 +8,7 @@ def start_search(search):
     a_key = "a"
     backspace_key = "backspace"
     enter_key = "enter"
-
+    pyautogui.sleep(2)
     for item in search:
         pyautogui.sleep(2)
         pyautogui.click(search_box_x_coord, search_box_y_coord)
@@ -20,5 +20,8 @@ def start_search(search):
 
 if __name__ == "__main__":
     search = ["how to turn off bing search history", "what is chatgpt", "leetcode", "codeforces", "Python documentation"
-              "what are the lastest news", "sports", "what is cubing", "how to get free microsoft points", "Hello world"]
+              "what are the lastest news", "sports", "what is cubing", "how to get free microsoft points", "Hello world", 
+              "bit manipilation", "binary trees", "segmentation trees", "graphs", "bfs", "dfs", "maths for cp", "cses fi", 
+              "sub sequence", "red coder on cf", "greedy algorithms", "binary search", "dynamic programming", "AIML", "SVM", 
+              "K means ", "KNN", "ANN", "CNN","hashmap in c++" ,"how to code like tourist"]
     start_search(search)
